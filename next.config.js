1a5ff4fb86c4e63e29db088e8dd7e3ca416b7bb3
Dimitar Germanov <germanov.dev@gmail.com>
2021-03-26 15:20:27 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,27 +1,9 @@
-// const withPlugins = require('next-compose-plugins');
-// const optimizedImages = require('next-optimized-images');
-module.exports = withPlugins([
-	// [
-	// 	optimizedImages,
-	// 	{
-	// 		mozjpeg: {
-	// 			quality: 80,
-	// 		},
-	// 		pngquant: {
-	// 			speed: 3,
-	// 			strip: true,
-	// 			verbose: true,
-	// 		},
-	// 		imagesPublicPath: '/weather/_next/static/images/',
-	// 	},
-	// ],
-	{
-		images: {
-			loader: 'imgix',
-			path: 'https://germanov.js.org/weather/',
-		},
-		basePath: '/weather',
-		assetPrefix: '/weather/',
-		env,
+module.exports = {
+	images: {
+		loader: 'imgix',
+		path: 'https://germanov.js.org/weather/',
 	},
-]);
+	basePath: '/weather',
+	assetPrefix: '/weather/',
+	env,
+};
