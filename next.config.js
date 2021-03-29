4f129478d184cfd68e7e7e17970c9ef430cc82dd
Dimitar Germanov <germanov.dev@gmail.com>
2021-03-29 13:31:28 +0300

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	// images: {
-	// 	loader: 'imgix',
-	// 	path: 'https://germanov.js.org/weather/',
-	// },
-	// basePath: '/weather',
-	// assetPrefix: '/weather/',
-	// imagesPublicPath: '/weather/_next/static/',
+	images: {
+		loader: 'imgix',
+		path: 'https://germanov.js.org/weather/',
+	},
+	basePath: '/weather',
+	assetPrefix: '/weather/',
+	imagesPublicPath: '/weather/_next/static/',
 	env: {
 		REACT_APP_WEATHER_API_KEY: process.env.REACT_APP_WEATHER_API_KEY,
 	},
